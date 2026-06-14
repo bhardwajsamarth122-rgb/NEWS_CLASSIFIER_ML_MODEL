87eb301be54508ae77ab3c43d11c048f4f440d10
Samarth Bhardwaj <bhardwajsamarth122@gmail.com>
2026-06-14 12:51:22 +0530

    final commit

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app = FastAPI(title="News Categorization")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://smart-news-card.lovable.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
